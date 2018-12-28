b996f4ab4e603617f675994dda3a6ed424ac8f47
fengliang <732419012@qq.com>
2018-12-28 09:58:36 +0800

    audio record and play

--- demo03/src/main/java/com/fltry/demo03/CameraActivity.java
+++ demo03/src/main/java/com/fltry/demo03/CameraActivity.java
@@ -3,20 +3,33 @@ package com.fltry.demo03;
 import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
-import android.graphics.Camera;
+import android.graphics.ImageFormat;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceHolder;
+import android.view.View;
 
 import com.fltry.demo03.databinding.ActivityCameraBinding;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class CameraActivity extends AppCompatActivity {
     private String[] permissions = new String[]{
@@ -24,6 +37,16 @@ public class CameraActivity extends AppCompatActivity {
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
     ActivityCameraBinding dataBinding;
+
+    private CameraManager manager;
+    private Handler childHandler, mainHandler;
+    private CameraDevice mCamera;
+    private CaptureRequest.Builder mPreviewBuilder;
+    private CameraCaptureSession mSession;
+    private ImageReader mImageReader;
+    // 创建拍照需要的CaptureRequest.Builder
+    private CaptureRequest.Builder captureRequestBuilder;
+    private SurfaceHolder holder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +57,30 @@ public class CameraActivity extends AppCompatActivity {
         init();
     }
 
+    @TargetApi(Build.VERSION_CODES.KITKAT)
     private void init() {
-        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        SurfaceHolder surfaceHolder = dataBinding.cameraSv.getHolder();
-        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+        manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        HandlerThread handlerThread = new HandlerThread("Camera2");
+        handlerThread.start();
+        mainHandler = new Handler(getMainLooper());
+        childHandler = new Handler(handlerThread.getLooper());
+        holder = dataBinding.cameraSv.getHolder();
+        holder.setKeepScreenOn(true);
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-
+                //获取可用相机设备列表
+                //打开相机
+                try {
+                    String[] CameraIdList = manager.getCameraIdList();
+                    if (ActivityCompat.checkSelfPermission(CameraActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    manager.openCamera(CameraIdList[0], mCameraDeviceStateCallback, mainHandler);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
@@ -53,7 +93,137 @@ public class CameraActivity extends AppCompatActivity {
 
             }
         });
+
+        //设置照片的大小
+        mImageReader = ImageReader.newInstance(ScreenUtils.getScreenWidth(CameraActivity.this),
+                ScreenUtils.dip2px(CameraActivity.this, 500), ImageFormat.JPEG, 2);
+        mImageReader.setOnImageAvailableListener(imageReader -> {
+            // 拿到拍照照片数据
+            Image image = imageReader.acquireNextImage();
+            ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+            byte[] bytes = new byte[buffer.remaining()];
+            buffer.get(bytes);//由缓冲区存入字节数组
+            image.close();
+            //saveBitmap(bytes);//保存照片的处理
+        }, mainHandler);
+
+        dataBinding.cameraBtn.setOnClickListener(v -> {
+            startActivity(new Intent(CameraActivity.this, CameraActivity2.class));
+        });
     }
+
+    /**
+     * 摄像头创建监听
+     */
+    private CameraDevice.StateCallback mCameraDeviceStateCallback = new CameraDevice.StateCallback() {
+        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onOpened(CameraDevice camera) {//打开摄像头
+            try {
+                //开启预览
+                mCamera = camera;
+                startPreview(camera);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            //关闭摄像头
+            if (mCamera != null) {
+                mCamera.close();
+                mCamera = null;
+            }
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            //发生错误
+        }
+    };
+
+    //开始预览，主要是camera.createCaptureSession这段代码很重要，创建会话
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void startPreview(final CameraDevice camera) throws CameraAccessException {
+        try {
+            // 创建预览需要的CaptureRequest.Builder
+            mPreviewBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            // 将SurfaceView的surface作为CaptureRequest.Builder的目标
+            mPreviewBuilder.addTarget(holder.getSurface());
+//            mPreviewBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_OFF);
+            mPreviewBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+            mPreviewBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_OFF);
+            //设置拍摄图像时相机设备是否使用光学防抖（OIS）。
+            mPreviewBuilder.set(CaptureRequest.LENS_OPTICAL_STABILIZATION_MODE, CaptureRequest.LENS_OPTICAL_STABILIZATION_MODE_ON);
+            //感光灵敏度
+            mPreviewBuilder.set(CaptureRequest.SENSOR_SENSITIVITY, 1600);
+            //曝光补偿//
+            mPreviewBuilder.set(CaptureRequest.CONTROL_AE_EXPOSURE_COMPENSATION, 0);
+            // 创建CameraCaptureSession，该对象负责管理处理预览请求和拍照请求
+            camera.createCaptureSession(Arrays.asList(holder.getSurface(), mImageReader.getSurface()), mSessionStateCallback, childHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 会话状态回调
+     */
+    private CameraCaptureSession.StateCallback mSessionStateCallback = new CameraCaptureSession.StateCallback() {
+        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onConfigured(CameraCaptureSession session) {
+            mSession = session;
+            if (mCamera != null && captureRequestBuilder == null) {
+                try {
+                    captureRequestBuilder = mCamera.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+                    // 将imageReader的surface作为CaptureRequest.Builder的目标
+                    captureRequestBuilder.addTarget(mImageReader.getSurface());
+                    //关闭自动对焦
+                    captureRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_OFF);
+                    captureRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_OFF);
+                    //设置拍摄图像时相机设备是否使用光学防抖（OIS）。
+                    captureRequestBuilder.set(CaptureRequest.LENS_OPTICAL_STABILIZATION_MODE, CaptureRequest.LENS_OPTICAL_STABILIZATION_MODE_ON);
+//                    captureRequestBuilder.set(CaptureRequest.SENSOR_SENSITIVITY, valueISO);
+                    //曝光补偿//
+                    captureRequestBuilder.set(CaptureRequest.CONTROL_AE_EXPOSURE_COMPENSATION, 0);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                updatePreview(session);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onConfigureFailed(CameraCaptureSession session) {
+
+        }
+    };
+
+    /**
+     * 更新会话，开启预览
+     *
+     * @param session
+     * @throws CameraAccessException
+     */
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void updatePreview(CameraCaptureSession session) throws CameraAccessException {
+        session.setRepeatingRequest(mPreviewBuilder.build(), mCaptureCallback, childHandler);
+    }
+
+    private CameraCaptureSession.CaptureCallback mCaptureCallback = new CameraCaptureSession.CaptureCallback() {
+        @Override
+        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+            super.onCaptureCompleted(session, request, result);
+            //需要连拍时，循环保存图片就可以了
+        }
+    };
 
     private void checkPermissions() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
---
+++ demo03/src/main/java/com/fltry/demo03/CameraActivity2.java
@@ -0,0 +1,221 @@
+package com.fltry.demo03;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.databinding.DataBindingUtil;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+
+import com.fltry.demo03.databinding.ActivityCamera2Binding;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+
+public class CameraActivity2 extends AppCompatActivity {
+    Size mPreviewSize;
+    Size mCaptureSize;
+    String mCameraId;
+    ImageReader mImageReader;
+    Handler mCameraHandler;
+    CameraManager mCameraManager;
+    CameraDevice mCameraDevice;
+
+    ActivityCamera2Binding dataBinding;
+    CaptureRequest.Builder mCaptureRequestBuilder;
+    CaptureRequest mCaptureRequest;
+    CameraCaptureSession mCameraCaptureSession;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_camera2);
+        dataBinding.cameraTv.setSurfaceTextureListener(mTextureListener);
+    }
+
+    private TextureView.SurfaceTextureListener mTextureListener = new TextureView.SurfaceTextureListener() {
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            //当SurefaceTexture可用的时候，设置相机参数并打开相机
+            setupCamera(width, height);
+            openCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void setupCamera(int width, int height) {
+        //获取摄像头的管理者CameraManager
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            //遍历所有摄像头
+            for (String cameraId : manager.getCameraIdList()) {
+                CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+                Integer facing = characteristics.get(CameraCharacteristics.LENS_FACING);
+                //此处默认打开后置摄像头
+                if (facing != null && facing == CameraCharacteristics.LENS_FACING_FRONT)
+                    continue;
+                //获取StreamConfigurationMap，它是管理摄像头支持的所有输出格式和尺寸
+                StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                assert map != null;
+                //根据TextureView的尺寸设置预览尺寸
+                mPreviewSize = getOptimalSize(map.getOutputSizes(SurfaceTexture.class), width, height);
+                //获取相机支持的最大拍照尺寸
+                mCaptureSize = Collections.max(Arrays.asList(map.getOutputSizes(ImageFormat.JPEG)),
+                        (lhs, rhs) -> Long.signum(lhs.getWidth() * lhs.getHeight() - rhs.getHeight() * rhs.getWidth()));
+                //此ImageReader用于拍照所需
+                setupImageReader();
+                mCameraId = cameraId;
+                break;
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //选择sizeMap中大于并且最接近width和height的size
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private Size getOptimalSize(Size[] sizeMap, int width, int height) {
+        List<Size> sizeList = new ArrayList<>();
+        for (Size option : sizeMap) {
+            if (width > height) {
+                if (option.getWidth() > width && option.getHeight() > height) {
+                    sizeList.add(option);
+                }
+            } else {
+                if (option.getWidth() > height && option.getHeight() > width) {
+                    sizeList.add(option);
+                }
+            }
+        }
+        if (sizeList.size() > 0) {
+            return Collections.min(sizeList, (lhs, rhs) -> Long.signum(lhs.getWidth() * lhs.getHeight() - rhs.getWidth() * rhs.getHeight()));
+        }
+        return sizeMap[0];
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void setupImageReader() {
+        //2代表ImageReader中最多可以获取两帧图像流
+        mImageReader = ImageReader.newInstance(mCaptureSize.getWidth(), mCaptureSize.getHeight(),
+                ImageFormat.JPEG, 1);
+        mImageReader.setOnImageAvailableListener(reader -> {
+            final Image image = reader.acquireNextImage();
+            runOnUiThread(() -> {
+                ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                byte[] bytes = new byte[buffer.remaining()];
+                buffer.get(bytes);//由缓冲区存入字节数组
+                image.close();
+            });
+        }, mCameraHandler);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void openCamera() {
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            if (ActivityCompat.checkSelfPermission(CameraActivity2.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mCameraManager.openCamera(mCameraId, mStateCallback, mCameraHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {
+            mCameraDevice = camera;
+            startPreview();
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            camera.close();
+            mCameraDevice = null;
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            camera.close();
+            mCameraDevice = null;
+        }
+    };
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void startPreview() {
+        SurfaceTexture mSurfaceTexture = dataBinding.cameraTv.getSurfaceTexture();
+        mSurfaceTexture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+        Surface previewSurface = new Surface(mSurfaceTexture);
+        try {
+            mCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            mCaptureRequestBuilder.addTarget(previewSurface);
+            mCameraDevice.createCaptureSession(Arrays.asList(previewSurface, mImageReader.getSurface()), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(CameraCaptureSession session) {
+                    try {
+                        mCaptureRequest = mCaptureRequestBuilder.build();
+                        mCameraCaptureSession = session;
+                        mCameraCaptureSession.setRepeatingRequest(mCaptureRequest, null, mCameraHandler);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(CameraCaptureSession session) {
+
+                }
+            }, mCameraHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
